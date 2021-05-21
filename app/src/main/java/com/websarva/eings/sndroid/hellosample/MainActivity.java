9c2f7f3b61ac859c604789b857a7ec6441c3b901
junnosuke789 <task.fuelex89@gmail.com>
2021-05-21 18:40:46 +0900

    P96 finish.

--- app/src/main/java/com/websarva/eings/sndroid/hellosample/MainActivity.java
+++ app/src/main/java/com/websarva/eings/sndroid/hellosample/MainActivity.java
@@ -3,6 +3,12 @@ package com.websarva.eings.sndroid.hellosample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.security.PrivateKey;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //リスナ設定
+        Button btClick = findViewById(R.id.btClick);
+        HelloListener listner = new HelloListener();
+
+        //ボタンにリスナを設定
+        btClick.setOnClickListener(listner);
     }
+    private class HelloListener implements View
+
+            .OnClickListener{
+        @Override
+        public void onClick(View view) {
+
+            
+            EditText input = findViewById(R.id.etName);
+            TextView output = findViewById(R.id.tvOutput);
+            String inputStr = input.getText().toString();
+            output.setText(inputStr + "さんこんにちは");
+
+        }
+    }
+
+
 }
